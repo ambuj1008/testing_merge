d35231385b79c915073dcbd6311f3cbc347afde4
ambuj <ambujdwivedi02@gmail.com>
2020-09-19 15:36:50 +0530

    function code

--- merge_job.sh
+++ merge_job.sh
@@ -25,26 +25,22 @@ SUB_MODULES=$(git config --file .gitmodules --get-regexp path | awk '{ print $2 
 
 #git submodule sync && git submodule update --init --recursive
 
-for SUB_MODULE in $SUB_MODULES; do	
-	if [ "$SUB_MODULE" == "$DMIAMSERVICE" ]; then
-	cd $SUB_MODULE;
-	echo "inside $SUB_MODULE"
+function gitCommandToMergeCode()
+{
 	#Checkout
 	git stash || exit 1
 	git checkout origin/master || exit 1
 	git fetch --all || exit 1
-	echo "rebase"
 	git rebase --onto tags/${RELEASE_TAG} origin/master || exit 1
-	echo "after rebase"
+
 	#Merge	
 	git merge --ff-only ${RELEASE_TAG} || exit 1
 	echo "Git log after merge:"
 	git log -n 5 || exit 1
 
 	#Pull
-	echo "pull"
 	git pull origin master || exit 1
-	echo "after pull"
+	
 	#Commit
 	COMMIT_MSG=$(git show dev --pretty=%B | cut -d '"' -f2)
 	if [[ $COMMIT_MSG != '' ]]; then
@@ -56,6 +52,12 @@ for SUB_MODULE in $SUB_MODULES; do
 	fi
 	#Push
 	git push origin HEAD:master || exit 1
+}
+
+for SUB_MODULE in $SUB_MODULES; do	
+	if [ "$SUB_MODULE" == "$DMIAMSERVICE" ]; then
+	cd $SUB_MODULE;
+		gitCommandToMergeCode
 	cd ..
 	fi
 done
