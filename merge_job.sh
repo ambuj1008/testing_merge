b8340adc785a0e674e47c8302605f66853635d8f
ambuj <ambujdwivedi02@gmail.com>
2020-09-19 15:40:21 +0530

    #calling function

--- merge_job.sh
+++ merge_job.sh
@@ -23,17 +23,43 @@ DMONBOARDING="onboarding-service";
 #Building Submodule List
 SUB_MODULES=$(git config --file .gitmodules --get-regexp path | awk '{ print $2 }')
 
-git submodule sync && git submodule update --init --recursive
+#git submodule sync && git submodule update --init --recursive
 
-for SUB_MODULE in $SUB_MODULES; do	
-	if [ "$SUB_MODULE" != "$DMIAMSERVICE" ] && [ "$SUB_MODULE" != "$DMONBOARDING" ]; then
-	cd $SUB_MODULE;
-	echo "inside $SUB_MODULE"
+function gitCommandToMergeCode()
+{
 	#Checkout
 	git stash || exit 1
 	git checkout origin/master || exit 1
-	git fetch origin || exit 1
-	#git rebase tags/${RELEASE_TAG} || exit 1
+	git fetch --all || exit 1
+	git rebase --onto tags/${RELEASE_TAG} origin/master || exit 1
+
+	#Merge	
+	git merge --ff-only ${RELEASE_TAG} || exit 1
+	echo "Git log after merge:"
+	git log -n 5 || exit 1
+
+	#Pull
+	git pull origin master || exit 1
+	
+	#Commit
+	COMMIT_MSG=$(git show dev --pretty=%B | cut -d '"' -f2)
+	if [[ $COMMIT_MSG != '' ]]; then
+	echo "Committing merge to origin/master"
+	git commit --amend -m "$COMMIT_MSG" || exit 1
+	else
+	echo "No commit message found";
+	exit 1
+	fi
+	#Push
+	git push origin HEAD:master || exit 1
+}
+
+gitCommandToMergeCode
+
+for SUB_MODULE in $SUB_MODULES; do	
+	if [ "$SUB_MODULE" == "$DMIAMSERVICE" ]; then
+	cd $SUB_MODULE;
+		gitCommandToMergeCode
 	cd ..
 	fi
 done
